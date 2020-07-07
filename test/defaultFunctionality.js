243f7167f8e7455096b5f1f54fd333b4a517bb34
Sergey Makhalov <sergymak.usa@gmail.com>
2020-07-06 17:30:57 -0700

    v.1.2

--- test/defaultFunctionality.js
+++ test/defaultFunctionality.js
@@ -19,27 +19,56 @@ describe('Default counter functionality', function () {
         expect(countValue).toEqual(expectedDCF.countValueTC041);
     })
 
+
     it('TC-042 LLF accept 1', function () {
-        inputNumber('left', expectedDCF.inputMin);
-        const result = $(selectorCnt.error).isDisplayed();
-        expect(result).toEqual(false);
-    })
+          inputNumber('left', expectedDCF.inputMin);
+          const result = $(selectorCnt.errorMsg).isDisplayed();
+          expect(result).toEqual(false);
+     })
+
 
     it('TC-043 ULF accept 9', function () {
         inputNumber('right', expectedDCF.inputMax);
-        const result = $(selectorCnt.error).isDisplayed();
+        const result = $(selectorCnt.errorMsg).isDisplayed();
         expect(result).toEqual(false);
+    })
 
-    /*
-        it('TC-044 LLF = 1 and ULF = 1 gives 2 black buttons', function () {
-             browser.refresh();
-            browser.pause(1000);
-            //inputNumber('left', expectedDCF.inputMin);
-            inputNumber('right', expectedDCF.inputMin);
-            const actual = $$(selectorCnt.blackBtn).filter(el => el.isDisplayed()).length;
-            expect(actual).toEqual(expectedDCF.countValueTC041);
-        })
-    */
+
+
+    // it('TC-044 LLF = 1 and ULF = 1 gives 2 black buttons', function () {
+    //     browser.refresh()
+    //     browser.pause(1000);
+    //     inputNumber('left', expectedDCF.inputMin);
+    //     //inputNumber('right', expectedDCF.inputMin);
+    //     const actual = $$(selectorCnt.blackBtn).filter(el => el.isDisplayed()).length;
+    //     expect(actual).toEqual(expectedDCF.countValueTC041);
+    // })
+
+
+
+
+    // it('TC-045 LLF = 9 and ULF = 9 gives 2 black buttons', function () {
+    //       browser.refresh();
+    //       inputNumber('right', expectedDCF.inputMax);
+    //       const actual = $$(selectorCnt.blackBtn).filter(el => el.isDisplayed()).length;
+    //       expect(actual).toEqual(expectedDCF.countValueTC045);
+    //     browser.pause(1000);
+    // })
+
+    it('TC-046 Reset button works', function () {
+        browser.refresh()
+        $$(selectorCnt.blackBtn)[0].click();
+        let countValue = $(selectorCnt.countValue).getText();
+        expect(countValue).toEqual(expectedDCF.countValueTC040);
+        //browser.pause(2000);
+        $(selectorCnt.resetBtn).click();
+        countValue = $(selectorCnt.countValue).getText();
+        expect(countValue).toEqual(expectedDCF.countValue);
+
+
+    })
+
+
 
 
 });
